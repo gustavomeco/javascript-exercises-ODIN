423feeff43ea259b3be72d5ee4ed9b991beb8243
Gustavomeco <82248764+gustavomeco@users.noreply.github.com>
2025-10-15 18:27:13 -0300

    feat: resolve two exercises from odin using loop

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reverseStr = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reverseStr += str.at(i)
+    }
+    return reverseStr
 };
-
+reverseString('hello there') // returns 'ereht olleh'
 // Do not edit below this line
 module.exports = reverseString;
