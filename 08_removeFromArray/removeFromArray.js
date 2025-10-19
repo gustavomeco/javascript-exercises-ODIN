309b7a5236a67ca91ac53ab97e6126aa489b3c35
Gustavomeco <82248764+gustavomeco@users.noreply.github.com>
2025-10-18 21:50:45 -0300

    feat: add a function that uses array method to resolve the exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-};
-
+const removeFromArray = function(arr, ...remove) {
+    const removeArr = Array.from(remove)
+    let newArr = [];
+    arr.forEach(item => {
+        if (!removeArr.includes(item)) {
+                newArr.push(item);
+        }
+    });
+    return newArr
+} 
+removeFromArray([1, 2, 3, 4], 3, 2)       
 // Do not edit below this line
 module.exports = removeFromArray;
