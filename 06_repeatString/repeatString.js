3c45340b734248a1a78c063e28cc3984d73fee29
Gustavomeco <82248764+gustavomeco@users.noreply.github.com>
2025-10-20 21:45:55 -0300

    feat: use logic and algorithm to resolve the problem

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    const str1 = str
+    for (let i = 1; i < num ; i++) {
+        str = str + str1;
+    }   
+    const test = num == 0 ? '' : num < 0 ? 'ERROR' : str ;
+    return test
 };
-
+repeatString('hey', 3) // returns 'heyheyhey'
 // Do not edit below this line
 module.exports = repeatString;
