001a64ad9652517400268365a3491d30659cb2c9
gustavomeco <82248764+gustavomeco@users.noreply.github.com>
2026-02-23 19:59:01 -0300

    fix: create a calculator using function and arrays

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  return numbers.reduce((initial,num) => initial + num,0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((initial,num) => initial * num,1);
 };
 
-const power = function() {
-	
+const power = function(n1,n2) {
+	return n1 ** n2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let numF = 1;
+	for (let i = num; i > 0 ; --i) {
+    numF *= i;
+  };
+  return numF;
 };
 
 // Do not edit below this line
