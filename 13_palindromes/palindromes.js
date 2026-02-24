47e8e6b35c10b875e763559977b8356faae39fe3
gustavomeco <82248764+gustavomeco@users.noreply.github.com>
2026-02-23 21:00:25 -0300

    fix: check if a string is a palindrome (hard)

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    //transformar em um array de cada letra e verificar com um loop se array.length eh igual array.i e ir diminuindo
+    const allowed = ("abcdefghijklmnopqrstuvwxyz0123456789")
+    const cleanStr = str.toLowerCase().split('').filter((char) => allowed.includes(char));
+    for (let i = 0; i < cleanStr.length / 2; i++) {
+        if (cleanStr[i] != cleanStr[cleanStr.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true
 };
 
 // Do not edit below this line
