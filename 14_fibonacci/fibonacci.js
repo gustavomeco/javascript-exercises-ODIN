1b1c9ffc5ffe076d21a4bf7ab1bf27d9813a03aa
gustavomeco <82248764+gustavomeco@users.noreply.github.com>
2026-02-25 20:41:59 -0300

    fibonacci using function

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    //vamos fazer um for ate o numero escolhido e criar uma conta dentro que adiciona numeros de forma fibonacci ate o numero escolhido
+    let x = 0;
+    let z = 0;
+    let y = 1;
+    if (num < 0) return ("OOPS");
+    if (num == 1) return (y);
+    for (let i = 1; i < num; i++) {
+        z = x + y;
+        x = y;
+        y = z;
+    }
+    return z;
 };
 
 // Do not edit below this line
