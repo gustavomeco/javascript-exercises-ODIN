1503b0d1e2b71fa3716e692d4f637c6fb2566991
gustavomeco <82248764+gustavomeco@users.noreply.github.com>
2026-02-25 20:48:28 -0300

    easy use array/map

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,17 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    const bookName = arr.map(arr => arr.title);
+    return bookName
 };
 
+const books = [
+  {
+    title: '1984',
+    author: 'Orwell'
+  },
+  {
+    title: 'Sherlock Holmes',
+    author: 'Arthur c.d'
+  }
+]
 // Do not edit below this line
 module.exports = getTheTitles;
