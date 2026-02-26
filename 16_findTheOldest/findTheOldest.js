28c0070354795a697fa07f123558e8827fd563a5
gustavomeco <82248764+gustavomeco@users.noreply.github.com>
2026-02-25 21:21:45 -0300

    fix: ultimo de objetos

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const oldestPerson = arr.sort((a,b) => {
+        if (a.yearOfDeath === undefined) {a.yearOfDeath = (new Date()).getFullYear()}
+        if (b.yearOfDeath === undefined) {b.yearOfDeath = (new Date()).getFullYear()}
+        const aAge = (a.yearOfDeath - a.yearOfBirth);
+        const bAge = (b.yearOfDeath - b.yearOfBirth);
+        return bAge - aAge;
+        }
+    )
+    return oldestPerson[0];
 };
 
 // Do not edit below this line
